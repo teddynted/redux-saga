943d284a304d719f774a6047317ff0c7ba19377a
teddymkekana <teddy@sov.tech>
2018-09-15 18:41:12 +0200

    Reducer refactor

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,14 +1,10 @@
-import { RECEIVE_POSTS, RECEIVE_DELETE, NEW_POST_SUCCESS, UPDATE_POST_SUCCESS } from '../actions/index';
+import { RECEIVE_POSTS, RECEIVE_DELETE } from '../actions/index';
 
 export default ( state = "", { type, data } ) => {
    switch(type){
       case RECEIVE_POSTS:
         return data;
       case RECEIVE_DELETE:
-        return data;
-      case NEW_POST_SUCCESS:
-        return data;
-      case UPDATE_POST_SUCCESS:
         return data;
       default: 
         return state;
